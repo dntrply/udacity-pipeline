062b825fee11b23dce445cea29fded7841dd1437
dntrplytch <dntrplytch@gmail.com>
2021-08-06 00:36:35 +0000

    Add files via upload

---
+++ endpoint.py
@@ -0,0 +1,73 @@
+import requests
+import json
+
+# URL for the web service, should be similar to:
+# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
+scoring_uri = 'http://30e78ff8-75bd-4807-9b36-e466df0fedad.southcentralus.azurecontainer.io/score'
+# If the service is authenticated, set the key or token
+key = 'f04WjZLuncUAav8IocWk6fN07aySvloB'
+
+# Two sets of data to score, so we get two results back
+data = {"data":
+        [
+          {
+            "age": 17,
+            "job": "blue-collar",
+            "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
+            "month": "may",
+            "day_of_week": "mon",
+            "duration": 971,
+            "campaign": 1,
+            "pdays": 999,
+            "previous": 1,
+            "poutcome": "failure",
+            "emp.var.rate": -1.8,
+            "cons.price.idx": 92.893,
+            "cons.conf.idx": -46.2,
+            "euribor3m": 1.299,
+            "nr.employed": 5099.1,
+          },
+          {
+            "age": 87,
+            "job": "blue-collar",
+            "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
+            "month": "may",
+            "day_of_week": "mon",
+            "duration": 471,
+            "campaign": 1,
+            "pdays": 999,
+            "previous": 1,
+            "poutcome": "failure",
+            "emp.var.rate": -1.8,
+            "cons.price.idx": 92.893,
+            "cons.conf.idx": -46.2,
+            "euribor3m": 1.299,
+            "nr.employed": 5099.1,
+          },
+      ]
+    }
+# Convert to JSON string
+input_data = json.dumps(data)
+with open("data.json", "w") as _f:
+    _f.write(input_data)
+
+# Set the content type
+headers = {'Content-Type': 'application/json'}
+# If authentication is enabled, set the authorization header
+headers['Authorization'] = f'Bearer {key}'
+
+# Make the request and display the response
+resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp.json())
+
+
